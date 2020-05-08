22644b19743b15f08fb8982ed5dc2db0257e588d
zkeBoy <985777468@qq.com>
2020-05-08 10:57:59 +0800

    Fix:新增字段

---
+++ EarthNews/Tool/Model/EVOUserCommunityDataObj.h
@@ -0,0 +1,24 @@
+//
+//  EVOUserCommunityDataObj.h
+//  EarthNews
+//
+//  Created by zkeBoy on 2020/5/8.
+//  Copyright © 2020 zkeBoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EVOUserCommunityDataObj : NSObject
+kSPrCopy__(NSString * ID);
+kSPrCopy__(NSString * Name);
+kSPrCopy__(NSString * Gender);
+kSPrCopy__(NSString * Age);
+kSPrCopy__(NSString * Nation);
+kSPrCopy__(NSString * Image);
+kSPrCopy__(NSString * Intrduce);
+kSPrCopy__(NSString * Image_1);
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EarthNews/Tool/Model/EVOUserCommunityDataObj.m
@@ -0,0 +1,13 @@
+//
+//  EVOUserCommunityDataObj.m
+//  EarthNews
+//
+//  Created by zkeBoy on 2020/5/8.
+//  Copyright © 2020 zkeBoy. All rights reserved.
+//
+
+#import "EVOUserCommunityDataObj.h"
+
+@implementation EVOUserCommunityDataObj
+
+@end
